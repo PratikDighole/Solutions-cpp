45eb9f193fc5cfd01f8ed6181ee5af4eb1935ce6
Symbol60 <72155336+PratikDighole@users.noreply.github.com>
2022-08-03 00:40:15 +0530

    Time: 4 ms (41.81%), Space: 8.5 MB (42.22%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -12,46 +12,15 @@
 class Solution {
 public:
     vector<int> v;
-    // void prior(TreeNode* r)
-    // {
-    //     if(r==NULL)return;
-    //     v.push_back(r->val);
-    //     prior(r->left);
-    //     prior(r->right);
-    // }
+    void prior(TreeNode* r)
+    {
+        if(r==NULL)return;
+        v.push_back(r->val);
+        prior(r->left);
+        prior(r->right);
+    }
     vector<int> preorderTraversal(TreeNode* root) {
         prior(root);
         return v;
-    }
-    void prior(TreeNode* r)
-    {
-        while(r)
-        {
-            
-            if(r->left==NULL)
-            {
-                v.push_back(r->val);
-                r=r->right;
-            }
-            else
-            {
-                TreeNode* curr=r->left;
-                while(curr->right && curr->right!=r)
-                {
-                    curr=curr->right;
-                }
-                if(curr->right!=NULL)
-                {
-                    curr->right=NULL;
-                    r=r->right;
-                    
-                }
-                else{
-                    curr->right=r;
-                    v.push_back(r->val);
-                    r=r->left;
-                }
-            }
-        }
     }
 };
