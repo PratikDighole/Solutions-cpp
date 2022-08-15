5486306fc851a3228ebde109c43b0fcdce4bf0ee
Symbol60 <72155336+PratikDighole@users.noreply.github.com>
2022-08-15 17:20:51 +0530

    Time: 1196 ms (5.02%), Space: 10.1 MB (96.37%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       vector<int>ans;                  
-       unordered_map<int,int>m;
-        
-        for(int i=0;i<nums.size();i++)    
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
         {
-            int val = target-nums[i];
-            if(m.find(val)!=m.end())
+            for(int j=i+1;j<nums.size();j++)
             {
-                ans.push_back(m.find(val)->second);
-                ans.push_back(i);
-                break;
+                if(nums[i]+nums[j]==target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
             }
-            m.insert(pair<int,int>(nums[i],i));  
         }
-        return ans; 
+        return ans;
     }
 };
